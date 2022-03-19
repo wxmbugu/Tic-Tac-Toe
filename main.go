f3b7488051dd04894d2f7ef9b1d543f5609af146
Wambug <wambugumacharia35@gmail.com>
2022-03-19 08:04:00 +0300

    draw functionality added

--- main.go
+++ main.go
@@ -11,15 +11,14 @@ import (
 func bmarker(board [9]string) [9]string {
 	fmt.Println("The board now looks like this:")
 	fmt.Println("-------")
-	fmt.Println("|" + board[0] + "|" + board[1] + "|" + board[2] + "|")
-	fmt.Println("|" + board[3] + "|" + board[4] + "|" + board[5] + "|")
-	fmt.Println("|" + board[6] + "|" + board[7] + "|" + board[8] + "|")
+	fmt.Println("| " + board[0] + "| " + board[1] + "| " + board[2] + "|")
+	fmt.Println("| " + board[3] + "| " + board[4] + "| " + board[5] + "|")
+	fmt.Println("| " + board[6] + "| " + board[7] + "| " + board[8] + "|")
 	fmt.Println("-------")
 	return board
-
 }
 func boardprinter() {
-	fmt.Println("Here is a board for you to make a mark:")
+	fmt.Println("Here is a board for you to make a mark using the specified letters:")
 	fmt.Println("-------")
 	fmt.Println("|a|b|c|")
 	fmt.Println("|d|e|f|")
@@ -32,9 +31,13 @@ func boardprinter() {
 //step 2.2:Saving into board all entries
 //Step 3:Players
 //Step 4:Checking rules of the game or winner and draws
-
 func placepiece(board *[9]string, position string, player string) {
 	var symbol string
+	winner, ok := win(board, player)
+	if ok == true {
+		fmt.Println("Winner is: ", winner)
+		os.Exit(1)
+	}
 	if player == "cpu" {
 		symbol = "X"
 		fmt.Println("Player X(comp) is playing... ")
@@ -50,6 +53,17 @@ func placepiece(board *[9]string, position string, player string) {
 			fmt.Fprintln(os.Stderr, err)
 		}
 		fmt.Println(position)
+	}
+	if board[0] != "" && board[1] != "" && board[2] != "" && board[3] != "" && board[4] != "" && board[5] != "" && board[6] != "" && board[7] != "" && board[8] != "" {
+		winner, ok := win(board, player)
+		if ok == true {
+			fmt.Println("Winner is: ", winner)
+			os.Exit(1)
+		} else {
+			fmt.Println("It's a draw!")
+			bmarker(*board)
+			os.Exit(1)
+		}
 	}
 	switch position {
 	case "a":
@@ -122,115 +136,22 @@ func placepiece(board *[9]string, position string, player string) {
 		break
 	}
 }
-
-func play() {
+func main() {
 	var ttt [9]string
 	var position string
 	var winner string
 	for {
+		boardprinter()
 		cpuPos := randomStr(1)
 		placepiece(&ttt, position, "player")
 		placepiece(&ttt, cpuPos, "cpu")
 		bmarker(ttt)
-
-		//Wins by matching the first row
-		if ttt[0] == ttt[1] {
-			if ttt[1] == ttt[2] {
-				if ttt[2] != "" {
-					winner = ttt[2]
-					fmt.Printf("Player,%s wins \n", winner)
-					break
-				}
-			}
-		}
-		//Wins by matching the middle  row
-		if ttt[3] == ttt[4] {
-			if ttt[4] == ttt[5] {
-				if ttt[3] != "" {
-					winner = ttt[3]
-					fmt.Printf("Player,%s wins \n", winner)
-					break
-				}
-
-			}
-		}
-		//Wins by matching the last row
-		if ttt[6] == ttt[7] {
-			if ttt[7] == ttt[8] {
-				if ttt[6] != "" {
-					winner = ttt[6]
-					fmt.Printf("Player,%s wins \n", winner)
-					break
-				}
-			}
-		}
-		//Wins by matching the left diagnol row
-		if ttt[0] == ttt[4] {
-			if ttt[4] == ttt[8] {
-				if ttt[0] != "" {
-					winner = ttt[0]
-					fmt.Printf("Player,%s wins \n", winner)
-					break
-				}
-			}
-		}
-		//Wins by matching the right diagnol row
-		if ttt[2] == ttt[4] {
-			if ttt[4] == ttt[6] {
-				if ttt[4] != "" {
-					winner = ttt[4]
-					fmt.Printf("Player,%s wins \n", winner)
-					break
-				}
-			}
-		}
-		//wins by matching the first column
-		if ttt[0] == ttt[3] {
-			if ttt[3] == ttt[6] {
-				if ttt[3] != "" {
-					winner = ttt[3]
-					fmt.Printf("Player,%s wins \n", winner)
-					break
-				}
-			}
-		} //wins by matching the middle column
-		if ttt[1] == ttt[4] {
-			if ttt[4] == ttt[7] {
-				if ttt[7] != "" {
-					winner = ttt[7]
-					fmt.Printf("Player,%s wins \n", winner)
-					break
-
-				}
-			}
-		}
-		//wins by matching the last column
-		if ttt[2] == ttt[5] {
-			if ttt[5] == ttt[8] {
-				if ttt[5] != "" {
-					winner = ttt[5]
-					fmt.Printf("Player,%s wins \n", winner)
-					break
-
-				}
-			}
-		}
-		if ttt[0] != "" && ttt[1] != "" && ttt[2] != "" && ttt[3] != "" && ttt[4] != "" && ttt[5] != "" && ttt[6] != "" && ttt[7] != "" && ttt[8] != "" {
-
-			fmt.Println("It's a draw!")
+		winner, ok := win(&ttt, winner)
+		if ok == true {
+			fmt.Println("Winner is: ", winner)
 			break
-
 		}
 	}
-
-}
-
-func main() {
-
-	boardprinter()
-
-	play()
-
 }
 
 //function to get random letters from (a-i)
@@ -245,5 +166,78 @@ func randomStr(length int) string {
 		output.WriteString(string(randomChar))
 	}
 	return output.String()
+}
 
+func win(ttt *[9]string, winner string) (string, bool) {
+	if ttt[0] == ttt[1] {
+		if ttt[1] == ttt[2] {
+			if ttt[2] != "" {
+				winner = ttt[2]
+				return winner, true
+			}
+		}
+	}
+	//Wins by matching the middle  row
+	if ttt[3] == ttt[4] {
+		if ttt[4] == ttt[5] {
+			if ttt[3] != "" {
+				return winner, true
+			}
+
+		}
+	}
+	//Wins by matching the last row
+	if ttt[6] == ttt[7] {
+		if ttt[7] == ttt[8] {
+			if ttt[6] != "" {
+				winner = ttt[6]
+				return winner, true
+			}
+		}
+	}
+	//Wins by matching the left diagnol row
+	if ttt[0] == ttt[4] {
+		if ttt[4] == ttt[8] {
+			if ttt[0] != "" {
+				winner = ttt[0]
+				return winner, true
+			}
+		}
+	}
+	//Wins by matching the right diagnol row
+	if ttt[2] == ttt[4] {
+		if ttt[4] == ttt[6] {
+			if ttt[4] != "" {
+				winner = ttt[4]
+				return winner, true
+			}
+		}
+	}
+	//wins by matching the first column
+	if ttt[0] == ttt[3] {
+		if ttt[3] == ttt[6] {
+			if ttt[3] != "" {
+				winner = ttt[3]
+				return winner, true
+			}
+		}
+	} //wins by matching the middle column
+	if ttt[1] == ttt[4] {
+		if ttt[4] == ttt[7] {
+			if ttt[7] != "" {
+				winner = ttt[7]
+				return winner, true
+			}
+		}
+	}
+	//wins by matching the last column
+	if ttt[2] == ttt[5] {
+		if ttt[5] == ttt[8] {
+			if ttt[5] != "" {
+				winner = ttt[5]
+				return winner, true
+			}
+		}
+	}
+	return "", false
 }
